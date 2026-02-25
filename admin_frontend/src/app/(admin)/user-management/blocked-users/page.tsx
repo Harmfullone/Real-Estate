8a1b69217cc32e8c7ea122b7122d6591bf8b5518
udayempire <kuday0726@gmail.com>
2026-02-25 13:32:57 +0530

    feat: add allUsers Table

--- admin_frontend/src/app/(admin)/user-management/blocked-users/page.tsx
+++ admin_frontend/src/app/(admin)/user-management/blocked-users/page.tsx
@@ -1,7 +1,49 @@
-export default function BlockedUsersPage() {
+import { AllUsersDataTable } from "@/components/user_management/allUsersDataTable"
+import { allUsersColumns, type UserColumnInterface, KYCStatus } from "@/components/user_management/allUsersColumns"
+import { BlockedUsersDataTable } from "@/components/user_management/blockedUsersDataTable";
+import { BlockedUsersColumns } from "@/components/user_management/blockedUsersColumns";
+
+async function getUsers(): Promise<UserColumnInterface[]> {
+    // const response = await fetch("https://api.example.com/users")
+    // const data = await response.json()
+    // return data;
+    return [
+        {
+            username: "Emilysadas White",
+            email: "ahhgdjsjh@gmail.com",
+            isVerified: true,
+            propertyListings: { total: 10, sold: 3, active: 4, unlisted: 3 },
+            gems: 54545,
+            kycStatus: "Verified",
+            isBlocked: false,
+        },
+        {
+            username: "amily White",
+            email: "ahhgdjsjh@gmail.com",
+            isVerified: false,
+            propertyListings: { total: 10, sold: 3, active: 4, unlisted: 3 },
+            gems: 10000,
+            kycStatus: "Pending",
+            isBlocked: true,
+        },
+        {
+            username: "Emily Whites",
+            email: "ahhgdjsjh@gmail.com",
+            isVerified: true,
+            propertyListings: { total: 10, sold: 3, active: 4, unlisted: 3 },
+            gems: 54545,
+            kycStatus: "Verified",
+            isBlocked: false,
+        },
+    ]
+}
+
+export default async function AllUsersPage() {
+    const data = await getUsers();
     return (
         <div>
-            <h1>Blocked Users</h1>
+            <BlockedUsersDataTable columns={BlockedUsersColumns} data={data} />
         </div>
     )
-}   
+}
+ 
